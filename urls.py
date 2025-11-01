e51ce76748efb24c3dd83058a26e56c9ba68022b
Muhammad Azmeer Ahmad <azmeerbaloch29@gmail.com>
2025-11-01 17:28:10 +0500

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('chat/', views.chat_api, name='chat_api'),  # ✅ correct route
-]
