3f7364629b3671cbb87be764b9b791588902828f
Muhammad Azmeer Ahmad <azmeerbaloch29@gmail.com>
2025-11-01 17:27:38 +0500

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class ChatMessage(models.Model):
-    user_message = models.BinaryField()      # Encrypted user message
-    bot_response = models.BinaryField()      # Encrypted bot response
-    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp
-
-    def __str__(self):
-        return f"Chat at {self.created_at}"
